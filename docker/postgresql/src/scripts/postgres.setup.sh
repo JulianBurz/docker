76f2331fc3c4fe57d9cfd4951e1d900b433b5ba4
Julian <julianburz@gmail.com>
2014-10-13 22:26:01 -0400

    bugfix for postgresql postgis error during setup

--- docker/postgresql/src/scripts/postgres.setup.sh
+++ docker/postgresql/src/scripts/postgres.setup.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Initializing PostgreSQL datadrive and PostGIS extension..."
+echo "Initializing PostgreSQL datadrive..."
 mkdir -p /var/datadrive/postgresql
 chmod -R 700 /var/datadrive/postgresql
 chown -R postgres:postgres /var/datadrive/postgresql
 sudo -u postgres /usr/lib/postgresql/9.3/bin/initdb -D /var/datadrive/postgresql
 sed 's,/var/lib/postgresql/9.3/main,/var/datadrive/postgresql,g' /etc/postgresql/9.3/main/postgresql.conf > tmpfile && mv tmpfile /etc/postgresql/9.3/main/postgresql.conf
 
-POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
-POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
-POSTGRESQL_SINGLE="sudo -u postgres $POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE"
-
-echo "Creating admin user and extensions..."
-$POSTGRESQL_SINGLE <<< "CREATE ROLE admin WITH LOGIN SUPERUSER CREATEDB CREATEROLE PASSWORD 'admin';" > /dev/null
-$POSTGRESQL_SINGLE <<< "CREATE EXTENSION postgis;" > /dev/null
-$POSTGRESQL_SINGLE <<< "CREATE EXTENSION postgis_topology;" > /dev/null
-$POSTGRESQL_SINGLE <<< 'CREATE EXTENSION "uuid-ossp";' > /dev/null
-
-echo "Importing dataset..."
-#/docker/src/scripts/postgres.build.sh
--- docker/postgresql/start-postgresql-build.sh
+++ docker/postgresql/start-postgresql-build.sh
@@ -13,4 +13,11 @@ sleep 10
 done
 sleep 10
 
+echo "Importing dataset..."
 /docker/src/scripts/postgres.build.sh
+
+echo "Creating admin user and extensions..."
+sudo -u postgres psql -q -c "CREATE ROLE admin WITH LOGIN SUPERUSER CREATEDB CREATEROLE PASSWORD 'admin';"
+sudo -u postgres psql -q -c "CREATE EXTENSION postgis;"
+sudo -u postgres psql -q -c "CREATE EXTENSION postgis_topology;"
+sudo -u postgres psql -q -c 'CREATE EXTENSION "uuid-ossp";'
